c35c3a4c7b79f65385d4504d2ff25dcc8ed9ff7b
Nikita <enagtim@mail.ru>
2025-01-14 18:02:47 +0300

    Свёрстан header

--- cat-app/src/widgets/header.tsx
+++ cat-app/src/widgets/header.tsx
@@ -1,11 +1,18 @@
 import { Outlet } from "react-router"
+import Tab from "../shared/ui/Tab/Tab"
+import styles from './header.module.css';
 
 function Header () {
     return (
-        <div>
-            <div>Header</div>
+        <>
+            <header className={styles.header}>
+                <nav className={styles.navigation}>
+                    <Tab url="/" name="Все котики" />
+                    <Tab url="/favorite-cats" name="Любимые котики"/>
+                </nav>
+            </header>
             <Outlet/>
-        </div>
+        </>
     )
 }
 export default Header
